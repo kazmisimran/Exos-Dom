201e781d70d4ba1e72d93908285c9bf757d5a8ce
Simran <kazmisimran@gmail.com>
2020-01-27 16:37:50 +0100

    exo2

--- javascript/exo2.js
+++ javascript/exo2.js
@@ -1,3 +1,6 @@
-function display {
-    
+let zap = () =>{
+    let btn2 = document.getElementById('btn2');
+    exo1.classList.add('d-none');
+    exo2.classList.remove('d-none');
 }
+btn2.addEventListener('click',zap);
